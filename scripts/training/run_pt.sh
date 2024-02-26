e32fcf88b07c9e197111d079da3a77aa698b9d12
EAG2014 <eag2014@live.com>
2024-02-26 23:06:42 +0800

    Update run_pt.sh

--- scripts/training/run_pt.sh
+++ scripts/training/run_pt.sh
@@ -18,7 +18,7 @@ output_dir=output_dir
 
 deepspeed_config_file=ds_zero2_no_offload.json
 
-torchrun --nnodes 1 --nproc_per_node 1 scripts/training/run_clm_pt_with_peft.py \
+torchrun --nnodes 1 --nproc_per_node 1 run_clm_pt_with_peft.py \
     --deepspeed ${deepspeed_config_file} \
     --model_name_or_path ${pretrained_model} \
     --tokenizer_name_or_path ${chinese_tokenizer_path} \
